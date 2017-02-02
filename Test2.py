24bd33c2323cdb65c6920bfda7298920e18a2b6a
MarcDekkert <marc98dekker@gmail.com>
2017-02-02 10:41:34 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -39,10 +39,10 @@ GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 #----------------------------------------
 '''Andere variabelen'''
-Grens_afstand = 20
+Grens_afstand = 25
 achteruit_tijd = 0.5
-omdraai_tijd = 1.1
-Uturn_tijd = 1.5
+omdraai_tijd = 0.6
+Uturn_tijd = 2
 Rondje_draaien = 8
 #----------------------------------------
 '''GPIO naar PWM-software naar frequentie in Hertz'''
@@ -186,7 +186,6 @@ try:
         	time.sleep(0.1)
         	if Dichtbij(Grens_afstand):
             		stop()
-			Sirene()
             		Uturn()
 		elif RijdtoverZwart():
 			stop()
